f90cd83bff70cfb67bafd4232cd425025e05cba3
Mustafa Genc <mustafagenc20@gmail.com>
2021-05-28 19:08:33 +0200

    Stream API

--- Chapter_19_Streaming_API/src/be/intecbrussel/Apple.java
+++ Chapter_19_Streaming_API/src/be/intecbrussel/Apple.java
@@ -1,19 +1,4 @@
 package be.intecbrussel;
 
 public class Apple {
-        private String name;
-
-        public Apple(String s){
-            this.name = s;
-        }
-        public String getName(){
-            return name;
-        }
-
-        @Override
-        public String toString() {
-            return "Apple{" +
-                    "name='" + name + '\'' +
-                    '}';
-        }
 }
