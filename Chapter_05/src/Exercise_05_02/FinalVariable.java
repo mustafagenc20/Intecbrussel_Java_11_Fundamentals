4ad5faf4426b22631136b0bf67eca74e8cca7cb2
Mustafa Genc <mustafagenc20@gmail.com>
2021-10-31 22:35:15 +0100

    initial commit

---
+++ Chapter_05/src/Exercise_05_02/FinalVariable.java
@@ -0,0 +1,10 @@
+package Exercise_05_02;
+
+public class FinalVariable {
+    public static void main(String[] args){
+        final double PI = 3.14;
+
+        // PI = 3.15; // Error: java cannot assign a value to final variable PI
+
+    }
+}
