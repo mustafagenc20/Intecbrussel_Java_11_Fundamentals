abb0207815ed10d3e49f6fbf11046c8cf3994ab8
Mustafa Genc <mustafagenc20@gmail.com>
2021-11-13 23:02:33 +0100

    initial commit

---
+++ Chapter_11_Enumeration/src/be/intecbrussel/enumerations/Color.java
@@ -0,0 +1,27 @@
+package be.intecbrussel.enumerations;
+
+public enum Color {
+    BLACK(0x00_00_00),
+    WHITE(0xFF_FF_FF),
+    RED(0xFF_00_00),
+    GREEN(0x00_FF_00),
+    BLUE(0x00_00_FF),
+    YELLOW(0xFF_FF_00);
+
+    private int rgb;
+
+    private Color(int rgb){
+        this.rgb = rgb;
+    }
+
+    public int getRgb() {
+        return rgb;
+    }
+
+    @Override
+    public String toString() {
+        return name() + "(" + rgb + ")";
+    }
+}
+
+
