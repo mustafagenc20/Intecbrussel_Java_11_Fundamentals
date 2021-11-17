9327c3ef12f3299f009259f2806dabcf26a66790
Mustafa Genc <mustafagenc20@gmail.com>
2021-11-17 21:35:58 +0100

    initial commit

---
+++ Chapter_13_Interfaces/src/be/intecbrussel/exercise03/PigApp.java
@@ -0,0 +1,11 @@
+package be.intecbrussel.exercise03;
+
+public class PigApp {
+    public static void main(String[] args) {
+        Pig pig = new MyPig();
+
+        pig.grunt();
+        pig.fly();
+
+    }
+}
