482054cee9b92ec38688ed704e6c3575cce28e69
Mustafa Genc <mustafagenc20@gmail.com>
2021-11-25 23:18:14 +0100

    initial commit

---
+++ Chapter_14_Nested_and_Anoniem_Classes/src/be/intecbrussel/exercise03_anoniem_class/Spotify.java
@@ -0,0 +1,11 @@
+package be.intecbrussel.exercise03_anoniem_class;
+
+public class Spotify {
+    public static void main(String[] args) {
+        Musician musician = new Musician();
+        musician.play("Violin");
+
+        MusicianReal musicianReal = new MusicianReal();
+        musicianReal.makeSound("Violin");
+    }
+}
