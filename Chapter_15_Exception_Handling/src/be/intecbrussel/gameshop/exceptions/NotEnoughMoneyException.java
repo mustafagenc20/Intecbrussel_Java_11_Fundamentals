5792fbda406e9279d45ea96eb82b555b7b13b685
Mustafa Genc <mustafagenc20@gmail.com>
2021-11-26 23:36:23 +0100

    initial commit

---
+++ Chapter_15_Exception_Handling/src/be/intecbrussel/gameshop/exceptions/NotEnoughMoneyException.java
@@ -0,0 +1,19 @@
+package be.intecbrussel.gameshop.exceptions;
+
+public class NotEnoughMoneyException extends GameShopException {
+    public NotEnoughMoneyException(){
+        super();
+    }
+
+    public NotEnoughMoneyException(String message){
+        super(message);
+    }
+
+    public NotEnoughMoneyException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public NotEnoughMoneyException(Throwable cause){
+        super(cause);
+    }
+}
