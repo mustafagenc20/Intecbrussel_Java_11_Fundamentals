4b7bf0f3c6df0b3fdfbca2e49b23ed988c0ef608
Mustafa Genc <mustafagenc20@gmail.com>
2021-11-26 23:35:50 +0100

    initial commit

---
+++ Chapter_15_Exception_Handling/src/be/intecbrussel/gameshop/exceptions/TooYoungToGameException.java
@@ -0,0 +1,19 @@
+package be.intecbrussel.gameshop.exceptions;
+
+public class TooYoungToGameException extends GameShopException{
+    public TooYoungToGameException(){
+        super();
+    }
+
+    public TooYoungToGameException(String message){
+        super(message);
+    }
+
+    public TooYoungToGameException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public TooYoungToGameException(Throwable cause){
+        super(cause);
+    }
+}
