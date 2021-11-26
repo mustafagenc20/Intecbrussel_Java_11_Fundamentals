f127340a1abec4591b5c9d7fd8024faeb5d3697e
Mustafa Genc <mustafagenc20@gmail.com>
2021-11-26 23:38:36 +0100

    initial commit

---
+++ Chapter_15_Exception_Handling/src/be/intecbrussel/gameshop/exceptions/EmptyStockException.java
@@ -0,0 +1,20 @@
+package be.intecbrussel.gameshop.exceptions;
+
+public class EmptyStockException extends GameShopException {
+    public EmptyStockException(){
+        super();
+    }
+
+    public EmptyStockException(String message){
+        super(message);
+    }
+
+    public EmptyStockException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public EmptyStockException(Throwable cause){
+        super(cause);
+    }
+
+}
