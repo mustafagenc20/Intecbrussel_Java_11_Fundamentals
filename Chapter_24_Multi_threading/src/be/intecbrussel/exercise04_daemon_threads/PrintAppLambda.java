b498d42623fa9a8a42e5e22f53b81e7ea5c408c7
Mustafa Genc <mustafagenc20@gmail.com>
2021-12-03 22:43:30 +0100

    initial commit

---
+++ Chapter_24_Multi_threading/src/be/intecbrussel/exercise04_daemon_threads/PrintAppLambda.java
@@ -0,0 +1,18 @@
+package be.intecbrussel.exercise04_daemon_threads;
+
+public class PrintAppLambda {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(()->print(ThreadColors.ANSI_BLUE + "1", 100));
+        Thread thread2 = new Thread(()->print(ThreadColors.ANSI_GREEN + "2", 1000));
+        thread2.setDaemon(true);
+        thread1.start();
+        thread2.start();
+    }
+
+    private static void print(String text, int count) {
+        for (int j = 0; j < count; j++) {
+            System.out.print(text);
+            Thread.yield();
+        }
+    }
+}
