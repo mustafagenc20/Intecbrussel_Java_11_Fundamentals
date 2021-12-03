20aaf93f272a36f6d4de90e543b9ced67a2b93aa
Mustafa Genc <mustafagenc20@gmail.com>
2021-12-03 22:42:53 +0100

    initial commit

---
+++ Chapter_24_Multi_threading/src/be/intecbrussel/exercise02_threads_with_lambda/PrintAppLambda.java
@@ -0,0 +1,21 @@
+package be.intecbrussel.exercise02_threads_with_lambda;
+
+public class PrintAppLambda {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(()->print('c', 100));
+        Thread thread2 = new Thread(()->print('*', 100));
+        System.out.println(thread1.getState());
+        System.out.println(thread2.getState());
+        thread1.start();
+        System.out.println(thread1.getState());
+        thread2.start();
+        System.out.println(thread2.getState());
+        print('&', 100);
+    }
+
+    public static void print(char c, int count){
+        for (int i = 0; i < count; i++) {
+            System.out.print(c);
+        }
+    }
+}
